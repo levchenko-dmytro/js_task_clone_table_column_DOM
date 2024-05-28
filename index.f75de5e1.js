481fc686d3f7273bad3dd7e41b0c2da2056e117e
Dmytro Levchenko <levchenkod91@gmail.com>
2024-05-28 19:48:20 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const table = document.querySelector("table");
+[
+    ...table.rows
+].forEach((row)=>{
+    const cell = row.cells[1];
+    const newCell = document.createElement(cell.tagName);
+    newCell.innerText = cell.innerText;
+    row.lastElementChild.before(newCell);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
